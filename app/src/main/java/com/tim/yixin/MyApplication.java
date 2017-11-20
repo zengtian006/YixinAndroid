40718b94ceb0d2f9405dbedf411c0b49dede7fac
Zeng <zengtian006@gmail.com>
2017-11-20 11:41:30 +0800

    tim

--- app/src/main/java/com/tim/yixin/MyApplication.java
+++ app/src/main/java/com/tim/yixin/MyApplication.java
@@ -20,7 +20,7 @@ public class MyApplication extends Application {
         //第一个参数默认设置为false, 即登录普通服务器, 如果设置为true, 则登录IP服务器,
         //注意: 当第一个参数设置为true的时候, 客服端需要选择登录ip服务器 才能够会话
         //正常情况下第一个参数请设置为false
-        KFAPIs.enableIPServerMode(false, this);
+//        KFAPIs.enableIPServerMode(false, this);
         //第一种登录方式，推荐
         KFAPIs.visitorLogin(this);
     }
